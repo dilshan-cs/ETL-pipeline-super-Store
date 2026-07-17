6e5cf7f0ffda22e02ccd6ec62ff99e75ad4077d1
dilshan-cs <kavishka.dilshan.cs@gmail.com>
2026-07-17 19:59:56 +0530

    made structure

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,68 +5,41 @@
 CREATE TABLE IF NOT EXISTS "DIM CUSTOMER" (
 
     "Customer Key" SERIAL PRIMARY KEY,
-
     "Customer ID" VARCHAR(20) NOT NULL,
-
     "Customer Name" VARCHAR(100) NOT NULL,
-
     "Segment" VARCHAR(20) NOT NULL
-
 );
-
 
 CREATE TABLE IF NOT EXISTS "DIM PRODUCT" (
 
     "Product Key" SERIAL PRIMARY KEY,
-
     "Product ID" VARCHAR(30) NOT NULL,
-
     "Product Name" VARCHAR(255) NOT NULL,
-
     "Category" VARCHAR(50) NOT NULL,
-
     "Sub-Category" VARCHAR(50) NOT NULL
-
 );
-
 
 CREATE TABLE IF NOT EXISTS "DIM LOCATION" (
 
     "Location Key" SERIAL PRIMARY KEY,
-
     "Postal Code" VARCHAR(20),
-
     "Country" VARCHAR(50),
-
     "Region" VARCHAR(50),
-
     "State" VARCHAR(50),
-
     "City" VARCHAR(50)
-
 );
-
 
 CREATE TABLE IF NOT EXISTS "DIM TIME" (
 
     "Time Key" SERIAL PRIMARY KEY,
-
     "Full Date" DATE NOT NULL UNIQUE,
-
     "Year" INTEGER,
-
     "Quarter" INTEGER,
-
     "Month" INTEGER,
-
     "Month Name" VARCHAR(20),
-
     "Day" INTEGER,
-
     "Day of Week" INTEGER,
-
     "Is Weekend" BOOLEAN
-
 );
 
 
@@ -77,27 +50,16 @@ CREATE TABLE IF NOT EXISTS "DIM TIME" (
 CREATE TABLE IF NOT EXISTS "FACT SALES" (
 
     "Row ID" INTEGER PRIMARY KEY,
-
     "Customer Key" INTEGER NOT NULL,
-
     "Product Key" INTEGER NOT NULL,
-
     "Location Key" INTEGER NOT NULL,
-
     "Order_Date_Key" INTEGER NOT NULL,
-
     "Ship_Date_Key" INTEGER NOT NULL,
-
     "Order ID" VARCHAR(30),
-
     "Ship Mode" VARCHAR(50),
-
     "Sales" NUMERIC(12,2),
-
     "Quantity" INTEGER,
-
     "Discount" NUMERIC(5,2),
-
     "Profit" NUMERIC(12,2),
 
     CONSTRAINT fk_customer
@@ -119,5 +81,4 @@ CREATE TABLE IF NOT EXISTS "FACT SALES" (
     CONSTRAINT fk_ship_time
         FOREIGN KEY ("Ship_Date_Key")
         REFERENCES "DIM TIME"("Time Key")
-
 );
